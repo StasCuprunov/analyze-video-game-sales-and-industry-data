c6ff20725229ee0dbebec141ccfc0016120eeefa
LordDiablicus <alexandersirotkin0209@gmail.com>
2026-03-30 19:20:55 +0200

    Kommentare von Stas 1,3-6

--- #sales_per_game.py
+++ #sales_per_game.py
@@ -4,32 +4,45 @@ import plotly.express as px
 from collections import defaultdict
 
 def sales_diagram(games, top_n=20): # erstellt das Diagramm für die top50 spiele
+    #Indezes als Variablen
+    INDEX_NA = 0
+    INDEX_JP = 1
+    INDEX_PAL = 2
+    INDEX_OTHER = 3
+
+    #Spaltennamen als Variablen
+    COL_GAME = "Spiel"
+    COL_NA = "NA"
+    COL_JP = "JP"
+    COL_PAL = "PAL"
+    COL_OTHER = "Other"
+    
     sales = defaultdict(lambda: [0, 0, 0, 0])  # [NA, JP, PAL, Other]
     
     for game in games:# geht alle spiele durch und summiert die verkaufszahlen auf
-        sales[game.title][0] += game.na_sales
-        sales[game.title][1] += game.jp_sales
-        sales[game.title][2] += game.pal_sales
-        sales[game.title][3] += game.other_sales
+        sales[game.title][INDEX_NA] += game.na_sales
+        sales[game.title][INDEX_JP] += game.jp_sales
+        sales[game.title][INDEX_PAL] += game.pal_sales
+        sales[game.title][INDEX_OTHER] += game.other_sales
 
     # berechnet den gesamtverkauf eines spiels
-    total_sales = {t: sum(v) for t, v in sales.items()}
+    total_sales = {title: sum(region.sales) for title, region.sales in sales.items()}
     top_titles = sorted(total_sales, key=total_sales.get, reverse=True)[:top_n]# wählt die top spiele aus
 
     df = pd.DataFrame({# erstellt das dataframe fürs plotly
         "Spiel": top_titles,
-        "NA": [sales[t][0] for t in top_titles],
-        "JP": [sales[t][1] for t in top_titles],
-        "PAL": [sales[t][2] for t in top_titles],
-        "Other": [sales[t][3] for t in top_titles],
+        "NA": [sales[t][INDEX_NA] for t in top_titles],
+        "JP": [sales[t][INDEX_JP] for t in top_titles],
+        "PAL": [sales[t][INDEX_PAL] for t in top_titles],
+        "Other": [sales[t][INDEX_OTHER] for t in top_titles],
     })
 
     fig = px.bar(# generiert das balkendiagramm
         df,
-        y="Spiel",# y-achse sind die spiele
-        x=["NA", "JP", "PAL", "Other"],#x-achse die regionen
+        y=COL_GAME,# y-achse sind die spiele
+        x=[COL_NA, COL_JP, COL_PAL, COL_OTHER]#x-achse die regionen
         orientation="h",# horizontale ausrichtung der balken
-        title="Verkäufe pro Spiel (gestapelt nach Regionen)",
+        title=f"Top {top_n} Spiele nach Verkaufszahlen (in Mio.)"
         text_auto=True   # sollte zahlen direkt im balken anzeigen
     )
 
